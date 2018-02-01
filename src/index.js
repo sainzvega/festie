37e14261164b2358071472527a9fc8b5977ecb36
Humberto Sainz <hsainz@talentize.com>
2018-01-31 19:14:35 -0800

    Remove App.css. Fix broken module references. Add HMR

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const rootEl = document.getElementById("root");
+
+let render = () => {
+  const App = require("app/layout/App").default;
+  ReactDOM.render( <App />, rootEl);
+};
+
+if(process.env.NODE_ENV !== "production") {
+  if(module.hot) {
+    module.hot.accept("app/layout/App", () => {
+      setTimeout(render);
+    });
+  }
+}
+
+render();
