a844051c065db0cbefb5f194a19b83bdb716bc8b
Humberto Sainz <hsainz@talentize.com>
2018-02-06 19:58:01 -0800

    Add helper packages. Create mock api to return artist by festival date

--- src/api/artists.js
+++ src/api/artists.js
@@ -1,5 +1,18 @@
-import data from 'data/artists.json';
+import artists from 'data/artists.json';
+import shows from 'data/shows.json';
+import { each } from 'lodash';
+import { format, isSameDay } from 'date-fns';
 
-export const fetchArtists = () => {
-    return data;
+const DATE_FORMAT = 'MM/DD/YYYY';
+export const getArtistsByShowDate = (date) => {
+    const formattedShowDate = format(date, DATE_FORMAT);
+    const filteredArtist = [];
+
+    each(shows, ({ artist_id, date_start }) => {
+        const showDate = format(date_start, DATE_FORMAT);
+        if (isSameDay(formattedShowDate, showDate))
+            filteredArtist.push(artists[artist_id]);
+    });
+
+    return filteredArtist;
 };
--- src/features/festival/Festival.js
+++ src/features/festival/Festival.js
@@ -1,37 +1,43 @@
 import React, { Component } from 'react';
 import FestivalHeader from 'features/festival/FestivalHeader'
 import FestivalDay from 'features/festival/FestivalDay';
-import { fetchArtists } from 'api/artists';
+import { getArtistsByShowDate } from 'api/artists';
 
 export class Festival extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            artists: [],
-            headerText: 'Festival Day'
+            artistsDay1: [],
+            artistsDay2: [],
+            artistsDay3: [],
+            headerDay1: 'Festival Day 1',
+            headerDay2: 'Festival Day 2',
+            headerDay3: 'Festival Day 3',
         };
     }
 
     componentDidMount() {
-        const artists = fetchArtists();
-        this.setState({ artists });
+        const artistsDay1 = getArtistsByShowDate('04-13-2018');
+        const artistsDay2 = getArtistsByShowDate('04-14-2018');
+        const artistsDay3 = getArtistsByShowDate('04-15-2018');
+        this.setState({ artistsDay1, artistsDay2, artistsDay3 });
     }
 
     render() {
-        const { artists, headerText } = this.state;
+        const { artistsDay1, artistsDay2, artistsDay3, headerDay1, headerDay2, headerDay3 } = this.state;
 
         return (
             <div className="max-width">
                 <FestivalHeader />
                 <br />
                 <br />
-                <FestivalDay headerText={headerText} artists={artists} />
+                <FestivalDay headerText={headerDay1} artists={artistsDay1} />
                 <br />
                 <br />
-                <FestivalDay headerText={headerText} artists={artists} />
+                <FestivalDay headerText={headerDay2} artists={artistsDay2} />
                 <br />
                 <br />
-                <FestivalDay headerText={headerText} artists={artists} />
+                <FestivalDay headerText={headerDay3} artists={artistsDay3} />
             </div>
         );
     }
